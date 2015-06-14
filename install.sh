cd8858fd8629197ea10619af6d5388242081d444
Jonathan Randall <jonathar@gmail.com>
2015-06-14 17:02:54 -0600

    Update install script to link .git-completion

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.git-completion ~/.git-completion
 
 git config --global init.templatedir '~/dotfiles/.git_template'
 git config --global alias.ctags '!.git/hooks/ctags'
