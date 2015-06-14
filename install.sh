2e92e0ae6adb0d9253097ae830256950f306a00d
Jonathan Randall <jonathar@gmail.com>
2015-06-14 16:57:26 -0600

    Adding tpopes ctag hooks for git

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+git config --global init.templatedir '~/dotfiles/.git_template'
+git config --global alias.ctags '!.git/hooks/ctags'
