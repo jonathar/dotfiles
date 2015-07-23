16e3bfadeb5894e1ffd47adde70cd9c7e1436293
Jonathan Randall <jonathar@gmail.com>
2015-07-23 13:18:48 -0600

    prevnt making snips symlink if already exists

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.git-completion ~/.git-completion
-ln -sf ~/dotfiles/snips ~/.vim/snips
+if [ ! -h ~/.vim/snips ]; then
+  ln -sf ~/dotfiles/snips ~/.vim/snips
+fi
 
 git config --global init.templatedir '~/dotfiles/.git_template'
 git config --global alias.ctags '!.git/hooks/ctags'
