3252d03bfbf5cadf772f274ee20af6c43b82fb51
jonathar <jonathar@users.noreply.github.com>
2017-12-02 18:15:30 -0700

    Improve installation

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@ mkdir -p ~/.config/nvim/
 mkdir -p ~/.config/nvim/dein
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 cp ~/dotfiles/fonts/* ~/Library/fonts/.
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s ~/.config/nvim/dein
+
+if [ ! -d "$HOME/.config/nvim/dein/repos/github.com/Shougo/dein.vim" ]; then
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s ~/.config/nvim/dein
+fi
+
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
@@ -25,3 +32,9 @@ vim +PluginInstall +qall
 
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 /usr/libexec/PlistBuddy -c "Set :\"New Bookmarks\":0:\"Normal Font\" \"SauceCodeProNerdFont---Regular 14\""  ~/Library/Preferences/com.googlecode.iterm2.plist
+
+if hash pip3 2>/dev/null; then
+  pip3 install --quiet -r ./requirements.txt
+else
+  echo "pip3 required to fully install"
+fi
